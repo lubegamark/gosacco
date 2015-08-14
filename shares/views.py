27b78c326e93fe66c7eab35c526e0b27293be335
mark <lubegamark@gmail.com>
2015-08-14 20:28:28 +0300

    Changed Savings endpoint to from generic.ListAPIView to APIView

--- shares/views.py
+++ shares/views.py
@@ -15,7 +15,7 @@ class ShareList(APIView):
 
     def get(self, request, format=None):
         """
-        Show are list of shares
+        Show a list of shares
         """
         shares = Shares.objects.all()
         serializer = SharesSerializer(shares,many=True)
@@ -34,9 +34,6 @@ class ShareList(APIView):
 
 
 class ShareDetail(APIView):
-    """
-    Handles all deltails about a users shares
-    """
     def get_member(self, pk):
         """
         Get a member.
@@ -48,33 +45,19 @@ class ShareDetail(APIView):
 
     def get(self,  request, pk, format=None):
         """
-        List members shares
+        Show Member's shares
         """
         member = self.get_member(int(pk))
         shares = Shares.get_members_shares(member, )
         serializer = SharesMinimalSerializer(shares, many=True)
         return Response(serializer.data)
 
-    def post(self, request, pk, format=None):
-        """
-        Add a new share for a user
-        """
-        member = self.get_member(pk)
-        new_share_details = request.data
-        share_type = ShareType.objects.get(pk=new_share_details['share_type'])
-        #serializer = SharesSerializer(data=request.data)
-        shares_added = SharePurchase.issue_shares(member, new_share_details['number_of_shares'], share_type)
-
-        if shares_added:
-            #serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 class ShareTypeList(APIView):
-    """
-    List the share types
-    """
     def get(self, request, format=None):
+        """
+        List the share types
+        """
         sharetype = ShareType.objects.all()
         serializer = ShareTypeSerializer(sharetype, many=True)
         return Response(serializer.data)
@@ -132,10 +115,9 @@ class ShareTransferList(APIView):
         except Member.DoesNotExist:
             raise Http404
 
-
     def get(self, request, pk, format=None):
         """
-        List of Share Transfers
+        List Share Transfer Transactions
         """
         if pk is not None:
             member = self.get_member(int(pk))
