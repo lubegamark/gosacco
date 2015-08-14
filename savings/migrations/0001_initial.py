dafbad0545eab2cf01860f0bcca4b66f629294bc
mark <lubegamark@gmail.com>
2015-08-14 19:54:36 +0300

    Added API endpoints for members

--- savings/migrations/0001_initial.py
+++ savings/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('members', '0001_initial'),
+        ('members', '0002_auto_20150715_1941'),
     ]
 
     operations = [
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('amount', models.IntegerField()),
-                ('date', models.DateTimeField()),
+                ('date', models.DateTimeField(auto_now_add=True)),
                 ('member', models.ForeignKey(to='members.Member')),
             ],
             options={
