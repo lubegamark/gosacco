b6d8e0c4b99b8805724151f4c1d4013bb5a63808
Mark Lubega <lubegamark@gmail.com>
2015-10-20 14:03:15 +0300

    Create Permissions for members.
    Create permissions to enable only owners of a resource or admins to access them.

---
+++ members/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework import permissions
+from members.models import Member
+from shares.models import ShareTransfer
+
+
+class IsOwnerOrAdmin(permissions.BasePermission):
+    """
+    Custom permission to only allow members to access it.
+    """
+    message = "You aren't allowed to access this."
+
+    def has_object_permission(self, request, view, obj):
+
+        # Members can
+        if request.method in ("GET", "POST", "HEAD",):
+            if isinstance(obj, Member):
+                return (obj == request.user.member) or request.user.is_staff
+            elif isinstance(obj, ShareTransfer):
+                return (obj.buyer == request.user.member or obj.seller == request.user.member) or request.user.is_staff
+            else:
+                return (obj.member == request.user.member) or request.user.is_staff
+        elif request.method in ("DELETE", "PUT",):
+            return request.user.is_staff
